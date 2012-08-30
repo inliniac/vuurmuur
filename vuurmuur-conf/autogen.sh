3a69805acc3cdc2ae8e83f86d0d96895a4a2ec57
Victor Julien <victor@inliniac.net>
2012-08-30 20:34:48 +0000

    vuurmuur-conf: autotools fixes to get make distcheck working

--- vuurmuur-conf/autogen.sh
+++ vuurmuur-conf/autogen.sh
@@ -10,7 +10,7 @@ rm -f .temp-gettextize
 # hack around an issue on Ubuntu 10.04: on the AC_OUTPUT line we'd see two
 # times a mention of intl/Makefile causing the commands below to fail.
 # Couldn't figure out how to make autotools behave, so hacking around it.
-sed -i 's/intl\/Makefile intl\/Makefile/intl\/Makefile/g' configure.in
+#sed -i 's/intl\/Makefile intl\/Makefile/intl\/Makefile/g' configure.in
 aclocal -I m4
 autoheader
 automake --copy --add-missing
