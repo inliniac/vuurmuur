2727cec9bcb50b959ea022f978c9e2a3eebfda0c
Victor Julien <victor@inliniac.net>
2012-08-30 20:54:48 +0000

    vuurmuur-conf: autotools fixes to get make distcheck working #2

--- vuurmuur-conf/autogen.sh
+++ vuurmuur-conf/autogen.sh
@@ -5,12 +5,12 @@ libtoolize --copy -f
 # ripped from: http://cvs.saout.de/lxr/saout/source/cryptsetup/setup-gettext
 sed 's:read .*< /dev/tty::' `which gettextize` > .temp-gettextize
 chmod +x .temp-gettextize
-echo n | ./.temp-gettextize --copy --force --intl --no-changelog || abort
+echo n | ./.temp-gettextize --copy --force --no-changelog || abort
 rm -f .temp-gettextize
 # hack around an issue on Ubuntu 10.04: on the AC_OUTPUT line we'd see two
 # times a mention of intl/Makefile causing the commands below to fail.
 # Couldn't figure out how to make autotools behave, so hacking around it.
-#sed -i 's/intl\/Makefile intl\/Makefile/intl\/Makefile/g' configure.in
+sed -i 's/intl\/Makefile intl\/Makefile/intl\/Makefile/g' configure.in
 aclocal -I m4
 autoheader
 automake --copy --add-missing
