b1e8014f2705974f462a2bd2cbe596763e503d19
Victor Julien <victor@inliniac.net>
2017-01-14 10:45:39 +0100

    WIP vuurmuur_log: connection log

---
+++ vuurmuur_log/conntrack.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *   Copyright (C) 2003-2017 by Victor Julien                              *
+ *   victor@vuurmuur.org                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef __CONNTRACK_H__
+#define __CONNTRACK_H__
+
+int conntrack_subscribe(struct vrmr_log_record *);
+int conntrack_disconnect(void);
+int conntrack_read(struct vrmr_log_record *);
+
+#endif /* __CONNTRACK_H__ */
+
